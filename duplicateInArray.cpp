7dc9bcc388ec43fd2ed8a8be2c32e9ac89282582
Farjana Ema Nishy <61512641+RIRUR-U@users.noreply.github.com>
2023-11-26 19:00:56 +0600

    Add files via upload

---
+++ duplicateInArray.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main()
+{
+     ll vs;
+     vector<ll> vec;
+     while(cin>>vs)
+     {
+         vec.push_back(vs);
+     }
+     cout<< vec.size()<<endl;
+//     for(int i=0;i<vec.size();i++)
+//     {
+//         cout<<vec[i]<<" ";
+//     }
+    return 0;
+}
