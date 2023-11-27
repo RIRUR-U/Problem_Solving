82ff5e53886b4fe7d1608f1cc5498883287a9ef1
Farjana Ema Nishy <143994678+Farjana-Gt@users.noreply.github.com>
2023-11-27 17:21:13 +0600

    Add files via upload

---
+++ sep10-2.cpp
@@ -0,0 +1,21 @@
+///1872A
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+
+        int a,b,c;
+        cin>>a>>b>>c;
+        double d;
+
+        d= (double(abs(a-b))/(2*c));
+        //cout<<d<<endl;
+        double r = ceil(d);
+        cout<<r<<endl;
+    }
+    return 0;
+}
