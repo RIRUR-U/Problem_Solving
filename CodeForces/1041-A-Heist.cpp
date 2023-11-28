531c4032925dd5152e585f87278e6a94009f14e1
Farjana Ema Nishy <61512641+RIRUR-U@users.noreply.github.com>
2023-11-28 11:06:05 +0600

    Add files via upload

--- CodeForces/1041-A-Heist.cpp
+++ CodeForces/1041-A-Heist.cpp
@@ -36,3 +36,21 @@ he objective is to iterate through possible scenarios to calculate the minimum n
     and here 9 & 11 are missing.
 
 */
+
+//Solution explanation (dry run sol)
+/* Initialize variables:
+
+n = 4
+Array a: 10 13 12 8
+cnt = 0
+Sort the array a in ascending order:
+
+Sorted array: 8 10 12 13
+Calculate the count of stolen keyboards in the sorted array:
+
+For i = 0: cnt += (10 - 8) - 1 = 1
+For i = 1: cnt += (12 - 10) - 1 = 1
+For i = 2: cnt += (13 - 12) - 1 = 0
+Output the result:
+
+cout << cnt << endl; => cout << 2 << endl; */
